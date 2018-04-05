628d180d6c91045aa4fa52f2f1dee4b89cfe2527
Charles Clayton <chaeclay@ut.utm.edu>
2018-04-05 17:05:14 -0500

    v2.2

--- test.js
+++ test.js
@@ -7,76 +7,110 @@ var scene;
 document.addEventListener("DOMContentLoaded", startBabylonJs, false);
 
 function startBabylonJs(){
+    
+    
 
     if(BABYLON.Engine.isSupported()){
         canvas = document.getElementById("renderCanvas");
         engine = new BABYLON.Engine(canvas, true);
-
-        scene = new BABYLON.Scene(engine);
-
-        var cam  = new BABYLON.FreeCamera("freecam", new BABYLON.Vector3(0,0, -10), scene);
-        cam.attachControl(canvas);
-        
-        // var simpleMesh = new BABYLON.Mesh("myMesh", scene);
-
-        // var plane = BABYLON.MeshBuilder.CreatePlane("plane", {}, scene);
-        // plane.applyToMesh(simpleMesh);
-
-        var cube = BABYLON.Mesh.CreateBox("cube", 2, scene);
-
-        var light = new BABYLON.PointLight("pLight", new BABYLON.Vector3(5, 10, -5));
-
-        engine.runRenderLoop(function(){
-            scene.render();
+    
+        window.addEventListener("resize", function(){
+            engine.resize();
         });
 
-    }
+        scene = new BABYLON.Scene(engine);
+        
+        //var cam  = new BABYLON.FreeCamera("freecam", new BABYLON.Vector3(0,2, -10), scene);
+        
+        var cam = new BABYLON.ArcRotateCamera("arcCam", 1, 0.8, 50, new BABYLON.Vector3(0,0,0), scene);
+        cam.attachControl(canvas);
+        cam.checkCollisions = true;
+        //cam.applyGravity = true;
 
+        // var assetManager = new BABYLON.AssetsManager(scene);
+        // var knucklesTask = assetManager.addMeshTask("knucklesTask", "", "./", "Knuckles.obj");
+        
+        // knucklesTask.onSuccess = function(task){
+        //     task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
+        // }
+
+        // knucklesTask.onError = function (task, message, exception){
+        //     console.log(message, exception);
+        // }
+        
+        // var simpleMesh = new BABYLON.Mesh("myMesh", scene);
+        
+        // var plane = BABYLON.MeshBuilder.CreatePlane("plane", {}, scene);
+        // plane.applyToMesh(simpleMesh);
+        
+        var cube = BABYLON.Mesh.CreateBox("cube", 2, scene);
+        
+        cube.position.y += 3;
+        
+        //cube.position.y += 5;
+        cube.checkCollisions = true;
+        
+        var light = new BABYLON.PointLight("pLight", new BABYLON.Vector3(5, 10, -5));
+        light.diffuse = BABYLON.Color3.Red();
+        
+        var hemi = new BABYLON.HemisphericLight("hLight", BABYLON.Vector3.Zero(), scene);
+        var ground = BABYLON.Mesh.CreateGround("floor", 100, 100, 100, scene);
+        ground.checkCollisions = true;
+        
+        engine.runRenderLoop(function(){
+            cube.rotation.x += 0.01;
+            cube.rotation.y += 0.01;
+            
+            scene.render();
+        });
+        
+    }
+    
 }
- 
+
 function CreatePlace(size){
     var indices = [];
     var positions= [];
     var normals = [];
     var uvs = [];
-
+    
     size = size || 1;
-
+    
     //Vertices
     var halfSize = size / 2.0;
     positions.push(-halfSize, -halfSize, 0);
     normals.push(0, 0, -1.0);
     uvs.push(0.0, 0.0);
-
-
+    
+    
     positions.push(halfSize, -halfSize, 0);
     normals.push(0, 0, -1.0);
     uvs.push(1.0, 0.0);
-
+    
     positions.push(halfSize, halfSize, 0);
     normals.push(0,0, -1.0);
     uvs.push(1.0, 1.0);
-
-
+    
+    
     //Indicies
     indices.push(0);
     indices.push(1);
     indices.push(2);
-
+    
     indices.push(0);
     indices.push(2);
     indices.push(3);
-
+    
     var vertexData = new BABYLON.VertexData();
-
+    
     vertexData.indices = indices;
     vertexData.positions = positions;
     vertexData.normals = normals;
     vertexData.uvs = uvs;
-
+    
     return vertexData;
-
-
+    
+    
 }
 
 
