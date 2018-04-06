fd4c92256bb59c0e413aed94cd000353d7ce438b
Charles Clayton <chaeclay@ut.utm.edu>
2018-04-06 13:12:10 -0500

    Add files via upload

--- test.js
+++ test.js
@@ -10,27 +10,27 @@ function startBabylonJs(){
     if(BABYLON.Engine.isSupported()){
         canvas = document.getElementById("renderCanvas");
         engine = new BABYLON.Engine(canvas, true);
-    
+        
         window.addEventListener("resize", function(){
             engine.resize();
         });
-
+        
         scene = new BABYLON.Scene(engine);
         
         //var cam  = new BABYLON.FreeCamera("freecam", new BABYLON.Vector3(0,2, -10), scene);
         
-        var cam = new BABYLON.ArcRotateCamera("arcCam", 1, 0.8, 50, new BABYLON.Vector3(0,0,0), scene);
+        var cam = new BABYLON.ArcRotateCamera("arcCam", 1, 0.8, 75, new BABYLON.Vector3(0,0,0), scene);
         cam.attachControl(canvas);
         cam.checkCollisions = true;
         //cam.applyGravity = true;
-
+        
         // var assetManager = new BABYLON.AssetsManager(scene);
         // var knucklesTask = assetManager.addMeshTask("knucklesTask", "", "./", "Knuckles.obj");
         
         // knucklesTask.onSuccess = function(task){
         //     task.loadedMeshes[0].position = BABYLON.Vector3.Zero();
         // }
-
+        
         // knucklesTask.onError = function (task, message, exception){
         //     console.log(message, exception);
         // }
@@ -39,62 +39,73 @@ function startBabylonJs(){
         
         // var plane = BABYLON.MeshBuilder.CreatePlane("plane", {}, scene);
         // plane.applyToMesh(simpleMesh);
+        
+        var base = BABYLON.Mesh.CreateSphere("base", 16, 25, scene);
+        var im = new BABYLON.StandardMaterial("space.jpeg", scene);
+        base.material = im;
+        //im.reflectionTexture.coordinatesMode = BABYLON.Texture.SPHERICAL_MODE;
+        im.bumpTexture = new BABYLON.Texture("space.jpeg", scene);
+        im.specularTexture = new BABYLON.Texture("space.jpeg", scene);
+        im.emissiveTexture = new BABYLON.Texture("space.jpeg", scene);
+        im.ambientTexture = new BABYLON.Texture("space.jpeg", scene);
+        //im.diffuseTexture.hasAlpha = true;
+        base.position.y += 6;
+        base.position.x -= 50;
+        base.position.z += 50;
+        
+        var numCube = 30;
+        
+        var bigCube = BABYLON.Mesh.CreateBox("bigCube", 12, scene);
+        bigCube.position.y += 6;
+        bigCube.position.x += 50;
+        bigCube.position.z -= 50;
+        var tex = new BABYLON.StandardMaterial("myMaterial", scene);
+        tex.bumpTexture = new BABYLON.Texture("world.png", scene);
+        tex.specularTexture = new BABYLON.Texture("world.png", scene);
+        tex.emissiveTexture = new BABYLON.Texture("world.png", scene);
+        tex.ambientTexture = new BABYLON.Texture("world.png", scene);
+        bigCube.material = tex;
 
-	var base = BABYLON.Mesh.CreateSphere("base", 16, 12, scene);
-	var im = new BABYLON.StandardMaterial("space.jpeg", scene);
-	base.material = im;
-	//im.reflectionTexture.coordinatesMode = BABYLON.Texture.SPHERICAL_MODE;
-	im.bumpTexture = new BABYLON.Texture("space.jpeg", scene);
-	im.specularTexture = new BABYLON.Texture("space.jpeg", scene);
-	im.emissiveTexture = new BABYLON.Texture("space.jpeg", scene);
-	im.ambientTexture = new BABYLON.Texture("space.jpeg", scene);
-	//im.diffuseTexture.hasAlpha = true;
-	base.position.y += 6;
-	base.position.x -= 50;
-	base.position.z += 50;
-
-	var numCube = 15;
-	
-
-	var cubes = [];
-	for(var i = 0; i < numCube; i++){
+        
+        var cubes = [];
+        for(var i = 0; i < numCube; i++){
             var cube = BABYLON.Mesh.CreateBox("", 2, scene);
-	var tex = new BABYLON.StandardMaterial("myMaterial", scene);
-	    tex.bumpTexture = new BABYLON.Texture("uganda.jpg", scene);
-	    tex.specularTexture = new BABYLON.Texture("uganda.jpg", scene);
-	    tex.emissiveTexture = new BABYLON.Texture("uganda.jpg", scene);
-	    tex.ambientTexture = new BABYLON.Texture("uganda.jpg", scene);
-	    cube.material=tex;
-            cube.position.y += 2;
-	    cube.position.x += 50;
-	    cube.position.z -= 50;
-	    cubes.push(cube);
-	}
-	
-	var makeCtr = 0;
-	var makeID = setInterval(function (){
-	    move(cubes[makeCtr]);
-	    makeCtr++;
-	    console.log('counter', makeCtr);
-	    if(makeCtr === numCube)
-		clearInterval(makeID);
-	}, 1500);
-
-
-	function move (cube){
-	    var moveCtr = 0;
-	    var moveid = setInterval(function(){
-		cube.position.x -= 1;
-		cube.position.z += 1;
-		moveCtr++;
-		if(moveCtr === 100){
-		    clearInterval(moveid);
-		}
-	    }, 500);
-	}
-	
-	
-	
+            var tex = new BABYLON.StandardMaterial("myMaterial", scene);
+            tex.bumpTexture = new BABYLON.Texture("uganda.jpg", scene);
+            tex.specularTexture = new BABYLON.Texture("uganda.jpg", scene);
+            tex.emissiveTexture = new BABYLON.Texture("uganda.jpg", scene);
+            tex.ambientTexture = new BABYLON.Texture("uganda.jpg", scene);
+            cube.material=tex;
+            cube.position.y += 1;
+            cube.position.x += 50;
+            cube.position.z -= 50;
+            cubes.push(cube);
+        }
+        
+        var makeCtr = 0;
+        var makeID = setInterval(function (){
+            move(cubes[makeCtr]);
+            makeCtr++;
+            console.log('counter', makeCtr);
+            if(makeCtr === numCube)
+            clearInterval(makeID);
+        }, 1500);
+        
+        
+        function move (cube){
+            var moveCtr = 0;
+            var moveid = setInterval(function(){
+                cube.position.x -= 1;
+                cube.position.z += 1;
+                moveCtr++;
+                if(moveCtr === 100){
+                    clearInterval(moveid);
+                }
+            }, 300);
+        }
+        
+        
+        
         //cube.position.y += 5;
         cube.checkCollisions = true;
         
@@ -105,15 +116,15 @@ function startBabylonJs(){
         var ground = BABYLON.Mesh.CreateGround("floor", 100, 100, 100, scene);
         ground.checkCollisions = true;
         var myMaterial = new BABYLON.StandardMaterial("myMaterial", scene);
-
-	
-	//myMaterial.specularColor = new BABYLON.Color3(0.5, 0., 0.);
-	myMaterial.emissiveColor = new BABYLON.Color3(1, 0, 0);
-	//myMaterial.ambientColor = new BABYLON.Color3(0.23, 0.98, 0.53);
-	ground.material = myMaterial;
+        
+        
+        //myMaterial.specularColor = new BABYLON.Color3(0.5, 0., 0.);
+        myMaterial.emissiveColor = new BABYLON.Color3(1, 0, 0);
+        //myMaterial.ambientColor = new BABYLON.Color3(0.23, 0.98, 0.53);
+        ground.material = myMaterial;
         engine.runRenderLoop(function(){
-        //    cube.rotation.x += 0.01;
-          //  cube.rotation.y += 0.01;
+            //    cube.rotation.x += 0.01;
+            //  cube.rotation.y += 0.01;
             
             scene.render();
         });
